cb3008ea8ade89fb07ab7118af17d1610191ce99
leo-trevisol <leotrevisol.2021@gmail.com>
2025-06-02 12:00:28 -0300

    Adicionando typeface nos itens do spinner dos graficos

--- app/src/main/java/com/br/leo/moodsnap/ui/dashboard/LabeledSpinnerAdapter.kt
+++ app/src/main/java/com/br/leo/moodsnap/ui/dashboard/LabeledSpinnerAdapter.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.br.leo.moodsnap.R
+import com.br.leo.moodsnap.ui.utils.FontUtils
 
 /**
  * Adaptador personalizado para spinners que inclui um rótulo antes do valor.
@@ -19,7 +21,10 @@ class LabeledSpinnerAdapter<T>(
     private val label: String,
     private val typeface: Typeface?,
     private val getItemText: (T) -> String
-) : ArrayAdapter<T>(context, R.layout.custom_comparison_spinner_item, items) {
+) : ArrayAdapter<T>(context, android.R.layout.simple_spinner_item, items) {
+
+    val sharedPreferences = context.getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+    val currentFont = sharedPreferences.getString("current_font", "default")
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context)
@@ -42,18 +47,16 @@ class LabeledSpinnerAdapter<T>(
         return view
     }
 
+
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = super.getDropDownView(position, convertView, parent)
         view.setBackgroundColor(ContextCompat.getColor(context, R.color.primary_background))
-        
+
         (view as TextView).apply {
             text = getItemText(items[position])
             setTextColor(ContextCompat.getColor(context, R.color.secundary))
-            if (typeface != null) {
-                this.typeface = typeface
-            }
+            typeface = ResourcesCompat.getFont(context, FontUtils.getFontResourceId(currentFont ?: "default"))
         }
-        
         return view
     }
 }
